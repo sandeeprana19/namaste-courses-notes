eabef47f3c864d39b8c8b514999670be012b1c30
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2025-05-11 23:37:05 +0530

    main
    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="app">
       <Header />
-      <div className="pt-28">
+      <div className="pt-28 pb-14 lg:py-28">
         <Outlet />
       </div>
     </div>
